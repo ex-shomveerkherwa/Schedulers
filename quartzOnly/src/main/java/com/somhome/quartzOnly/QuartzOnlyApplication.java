0a254f546a233e595d8f110f91fb2857d8ebe117
SK-aped <som.kherwa@momentoustech.com>
2020-07-20 13:58:15 +0530

    Schedule with only quartz lib

---
+++ quartzOnly/src/main/java/com/somhome/quartzOnly/QuartzOnlyApplication.java
@@ -0,0 +1,13 @@
+package com.somhome.quartzOnly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzOnlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzOnlyApplication.class, args);
+	}
+
+}
